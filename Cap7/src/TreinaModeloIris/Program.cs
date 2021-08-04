8de105f6702cfb5486301d631748fdb15edf9a4a
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-08-03 23:07:33 -0300

    Configuraçao do dataset

--- Cap7/src/TreinaModeloIris/Program.cs
+++ Cap7/src/TreinaModeloIris/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.ML;
 
 namespace TreinaModeloIris
 {
     class Program
     {
+        private static readonly string _dataPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Dados", "iris-full.txt");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Iniciando programa");
+
+            // Primeira etapa: Carregar os dados
+            if (!File.Exists(_dataPath))
+            {
+                Console.WriteLine("Dataset não encontrado");
+                Console.ReadKey();
+                return;
+            }
+
+            var mlContext = new MLContext(seed: 1234);
+
+            Console.WriteLine("Finalizando programa");
+            Console.ReadKey();
         }
     }
 }
