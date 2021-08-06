db5a0626ca3d9fd4973a079ea171b972cfb04287
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-08-05 21:34:30 -0300

    Desserializaçao de Modelos (Carregando Modelos)

--- Cap7/src/PredicaoIris/Program.cs
+++ Cap7/src/PredicaoIris/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.IO;
+using Microsoft.ML;
+using System.Reflection;
 
 namespace PredicaoIris
 {
     class Program
     {
+        private static readonly string _modelPath = 
+            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Modelos", "Modelo.tar");
+
+        private static readonly MLContext _mlContext = new MLContext();
+
+        private static ITransformer _trainedModel;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Iniciando programa");
+            Load();
+            Console.WriteLine("Finalizando programa");
+        }
+
+        private static void Load()
+        {
+            using var fs = new FileStream(_modelPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            _trainedModel = _mlContext.Model.Load(fs);
         }
     }
 }
