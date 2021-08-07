97430058c5a1bac661a1af133e7fc0ea1e7898b7
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-08-06 21:45:08 -0300

    Configuraçao Inicial do Projeto

--- Cap8/src/BicicletaModeloTreino/Program.cs
+++ Cap8/src/BicicletaModeloTreino/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-
+using System.IO;
+using System.Reflection;
+using  Microsoft.ML;
 namespace BicicletaModeloTreino
 {
     class Program
     {
+        private static readonly string _dadosTreinoPath =
+            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Dados", "demanda_bike_hora_treino.csv");
+
+        private static readonly string _dadosTestePath =
+            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Dados", "demanda_bike_hora_teste.csv");
+
+        private static MLContext _mlContext;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (!File.Exists(_dadosTreinoPath) || !File.Exists(_dadosTreinoPath))
+            {
+                Console.WriteLine("Datasets não encontrados!");
+                return;
+            }
+
+            _mlContext = new MLContext(seed: 34);
         }
     }
 }
