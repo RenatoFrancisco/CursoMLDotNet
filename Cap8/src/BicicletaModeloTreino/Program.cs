a42dfef879a90acd3c80bfd4c973300f24f50733
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-08-06 21:17:11 -0300

    Criaçao de Projeto para Treinamento de Modelo

---
+++ Cap8/src/BicicletaModeloTreino/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BicicletaModeloTreino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
