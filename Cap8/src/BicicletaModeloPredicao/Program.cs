8b1306d51b01c57922be602c16359ab73b18c616
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-08-10 22:59:27 -0300

    Carga do Modelo

--- Cap8/src/BicicletaModeloPredicao/Program.cs
+++ Cap8/src/BicicletaModeloPredicao/Program.cs
@@ -14,11 +14,32 @@ namespace BicicletaModeloPredicao
 
         private static MLContext _mlContext;
 
+        private static readonly string[] _nomesAlgoritmos = new string[] { "FastTree", "SDCA" };
+
         static void Main(string[] args)
         {
             var exemplos = BikeHorasInstanciaExemplo.GetExemplos();
 
             Console.WriteLine($"Total de exemplos: {exemplos.Count}");
+
+            foreach (var algoritmo in _nomesAlgoritmos)
+            {
+                var modelFilePath = Path.Combine(_modelDirectoryPath, algoritmo, ".tar");
+                if (!File.Exists(modelFilePath))
+                {
+                    Console.WriteLine($"Modelo não encontrado: {modelFilePath}");
+                    Console.ReadKey();
+                    return;
+                }
+
+                using var fs = new FileStream(
+                    modelFilePath, 
+                    FileMode.Open, 
+                    FileAccess.Read, 
+                    FileShare.Read);
+
+                var trainedModel = _mlContext.Model.Load(fs);
+            }
 
             _mlContext = new MLContext(seed: 34);
         }
