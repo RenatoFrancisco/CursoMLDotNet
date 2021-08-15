2ef626fadce2fb6fe43ff840f69dac308af67579
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-08-15 15:30:43 -0300

    Treinamento do Modelo

--- Cap9/src/Clusterizacao/Program.cs
+++ Cap9/src/Clusterizacao/Program.cs
@@ -10,7 +10,7 @@ namespace Clusterizacao
     class Program
     {
         private static string _dadosPath = 
-            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Dados", "iris-full");
+            Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Dados", "iris-full.txt");
 
         private static MLContext _mlContext;
 
@@ -31,13 +31,31 @@ namespace Clusterizacao
                 new TextLoader.Column(nameof(IrisData.SepalLength), DataKind.Single, 1),
                 new TextLoader.Column(nameof(IrisData.SepalWidth), DataKind.Single, 2),
                 new TextLoader.Column(nameof(IrisData.PetalLength), DataKind.Single, 3),
-                new TextLoader.Column(nameof(IrisData.PetalLength), DataKind.Single, 4),
+                new TextLoader.Column(nameof(IrisData.PetalWidth), DataKind.Single, 4),
             };
 
             // Carga do arquivo
             var dataViewDados = 
                 _mlContext.Data.LoadFromTextFile(_dadosPath, columns: colunas, separatorChar: '\t', hasHeader: true);
 
+            // Divisão do dados
+            var dadosTreinoTeste = _mlContext.Clustering.TrainTestSplit(dataViewDados, testFraction: 0.25);
+
+            // Concatenação das colunas
+            var pipelineProcessamento = 
+                _mlContext.Transforms.Concatenate(
+                    DefaultColumnNames.Features,
+                    nameof(IrisData.SepalLength),
+                    nameof(IrisData.SepalWidth),
+                    nameof(IrisData.PetalLength),
+                    nameof(IrisData.PetalWidth));
+
+            var trainer = 
+                _mlContext.Clustering.Trainers.KMeans(featureColumnName: DefaultColumnNames.Features, clustersCount: 3);
+
+            var pipelineTreinamento = pipelineProcessamento.Append(trainer);
+
+            var trainedModel =  pipelineTreinamento.Fit(dadosTreinoTeste.TrainSet);
         }
     }
 }
